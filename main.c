24850bccc9b62946fb1468c1d8c0cf97de29324a
Nguyen Minh Tuan <tuannm88.hni@gmail.com>
2022-05-30 20:08:09 +0700

    Create main.c
    Main code C

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Welcome to C!\n");
+		return 0; /*indicate that program ended successfully*/
+} /* end function main*/
