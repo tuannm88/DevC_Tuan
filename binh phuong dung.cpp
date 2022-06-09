ccdb9febddcd3078980d1cbb3a6370f11038f603
Nguyễn Minh Tuấn <106530588+tuannm88@users.noreply.github.com>
2022-06-09 16:13:23 +0700

    Add files via upload

---
+++ binh phuong dung.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a, b;
+	printf("nhap vao so a = ");
+	scanf("%d",&a);
+
+	b=pow(a,2);
+	printf("binh phuong cua a = b = %d",b);
+	return 0;
+}
